959444e01b2c0eacbd06e9d156e6e002e86b0952
bartolo1024 <bartlomiej.olechno@gmail.com>
2020-07-15 15:30:22 +0200

    endlines docstring

--- explib/enqueue.py
+++ explib/enqueue.py
@@ -2,6 +2,20 @@ import click
 from . import experiment
 
 
+def test_method():
+  """
+  This is a test method
+
+  | Arguments:
+  | arg1: arg1 description
+  | arg2: arg2 description
+
+  | Returns:
+  | None
+  """
+  print('I am a test method')
+
+
 @click.command()
 def main(*args, **kwargs):
     """Some docs xyz"""
